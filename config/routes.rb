5afc00ce61d053588d289f32db420e7f43e8b8a0
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-01-31 14:38:07 -0200

    Configures GraphQL

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount GraphiQL::Rails::Engine, at: "/gq",
+                         graphql_path: "/graphql" if Rails.env.development?
+
   post "/graphql", to: "graphql#execute"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
